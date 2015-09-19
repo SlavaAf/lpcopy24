1be162e86ea2a8c61e9d3b579f2734176d54c2ad
roman <rgorkovenko@gmail.com>
2015-09-19 10:24:19 +0600

    Добавлена проверка на валидность url и существование скачиваемого сайта

--- home/Downloader.py
+++ home/Downloader.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
+import fnmatch
 import tarfile, os, subprocess
-import sqlite3
 import random
 import string
 
@@ -8,23 +8,49 @@ import string
 class SiteParser:
     arch_folder = " --directory-prefix=static/sites"
     # command = 'wget.sh '
-    command = '/usr/local/bin/wget -r -l10 -k -p -E -nc '
+    # command = '"C:/Program Files (x86)/GnuWin32/bin/wget.exe" -r -k -p -E -nc '
+    command = '/usr/bin/wget -r -l10 -k -p -E -nc '
 
     def __init__(self):
         pass
 
     def save_site(self, url):
+        #Download site
         folder = ''.join(
             random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(16))
         print(folder)
+        print(url)
         os.mkdir("static/sites/" + folder)
         subprocess.call(self.command + url + self.arch_folder + '/' + folder, shell=True)
 
         dirs = os.listdir("static/sites/" + folder)
 
+        #Create tarfile
         if len(dirs) > 0:
             tar = tarfile.open("static/tars/" + folder + ".tar.gz", "w:gz")
             tar.add("static/sites/" + folder + '/' + dirs[0] + "/", folder)
 
             tar.close()
             print("end")
+
+            #Get path to index.html
+            index_files = self.find("index.html", "static\\sites\\" + folder)
+            if len(index_files) > 0:
+                print(index_files[0])
+            else:
+                print("index.html not found")
+
+            return "Download site completed"
+        else:
+            os.rmdir("static/sites/" + folder)
+            return "Site not found"
+
+        #Add site to database
+
+    def find(self, pattern, path):
+        result = []
+        for root, dirs, files in os.walk(path):
+            for name in files:
+                if fnmatch.fnmatch(name, pattern):
+                    result.append(os.path.join(root, name))
+        return result
--- home/views.py
+++ home/views.py
@@ -11,9 +11,19 @@ def index_view(request):
     if request.method == 'POST' and form.is_valid():
         parser = SiteParser()
         url = request.POST.get('url')
-        parser.save_site(url)
+        url = url_validate(url)
 
+        parser.save_site(url)
         #
         return redirect('/')
     else:
         return render(request, 'index.html', {'form': form})
+
+
+def url_validate(url):
+    if str(url).find("http://") >= 0 or str(url).find("https://") >= 0:
+        pass
+    else:
+        url = "http://"+url
+        return url
+    return url
