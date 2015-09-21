af43926009cd30106827da7585d160042651d39c
SlavaAf <crazzy.dead@gmail.com>
2015-09-21 20:41:27 +0600

    add  block modal window for base template, ready url for download sites

--- static/js/contact_me.js
+++ static/js/contact_me.js
@@ -1,4 +1,5 @@
-$(function() {
+$("#sale_b").click(function() {
+//$(function() {
 
     $("input").jqBootstrapValidation({
         preventSubmit: true,
@@ -7,9 +8,9 @@ $(function() {
         },
         submitSuccess: function($form, event) {
             // Prevent spam click and default submit behaviour
-            $("#btnSubmit").attr("disabled", true);
+            $("#sale_b").attr("disabled", true);
             event.preventDefault();
-            
+
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
@@ -31,7 +32,72 @@ $(function() {
                 //cache: false,
                 success: function() {
                     // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
+                    $("#sale_b").attr("disabled", false);
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Ваше сообщение отправлено. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Простите " + firstName + ", возникла ошибка при отправке. Попробуйте позже!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                }
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        }
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+$("#sale_m").click(function() {
+    $("input").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            // Prevent spam click and default submit behaviour
+            $("#sale_m").attr("disabled", true);
+            event.preventDefault();
+
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            //post_form_json(name, url, email);
+            $.ajax({
+                type: 'get',
+                url: '/ajax_post_modal/',
+                data: {
+                    name: name,
+                    email: email
+                },
+                //cache: false,
+                success: function() {
+                    // Enable button & show success message
+                    $("#sale_m").attr("disabled", false);
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
