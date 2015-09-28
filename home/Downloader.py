d8c95eb129bb2a847079ffd1b4fe5ea8a23a8c8f
SlavaAf <crazzy.dead@gmail.com>
2015-09-29 01:14:11 +0600

    # coding: utf-8

--- home/Downloader.py
+++ home/Downloader.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# coding: utf-8
 import fnmatch
 import tarfile, os, subprocess, zipfile, tempfile
 import random
--- lpcopy24/settings.py
+++ lpcopy24/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    'gunicorn',
 
 )
 
