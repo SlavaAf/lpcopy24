0921e090f61a001ffe667c3fa91ff49e6929fe99
SlavaAf <crazzy.dead@gmail.com>
2015-09-29 13:09:17 +0600

    form replacer in base and index_base

--- static/js/modal_order.js
+++ static/js/modal_order.js
@@ -1,1 +1,123 @@
-$(function(){$("input").jqBootstrapValidation({preventSubmit:!0,submitError:function(t,s,e){},submitSuccess:function(t,s){$("#sale_m").attr("disabled",!0),s.preventDefault();var e=$("input#name").val(),a=$("input#email").val(),n=$("#sale_m").parent().attr("id"),c=e;c.indexOf(" ")>=0&&(c=e.split(" ").slice(0,-1).join(" ")),$.ajax({type:"get",url:"/ajax_post_modal/",data:{name:e,email:a,pk:n},success:function(){$("#btnSubmit").attr("disabled",!1),$("#success").html("<div class='alert alert-success'>"),$("#success > .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>"),$("#success > .alert-success").append("<strong>Ваше сообщение отправлено. </strong>"),$("#success > .alert-success").append("</div>"),$("#contactForm").trigger("reset")},error:function(){$("#success").html("<div class='alert alert-danger'>"),$("#success > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>"),$("#success > .alert-danger").append("<strong>Простите "+c+", возникла ошибка при отправке. Попробуйте позже!"),$("#success > .alert-danger").append("</div>"),$("#contactForm").trigger("reset")}})},filter:function(){return $(this).is(":visible")}}),$('a[data-toggle="tab"]').click(function(t){t.preventDefault(),$(this).tab("show")})}),$("#name").focus(function(){$("#success").html("")});
+$(function() {
+
+    $("input").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            // Prevent spam click and default submit behaviour
+            $("#sale_m").attr("disabled", true);
+            event.preventDefault();
+            
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var pk_s = $('#sale_m').parent().attr('id');
+            var firstName = name; // For Success/Failure Message
+            var t_t = false;
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            //post_form_json(name, url, email);
+            if("btn-link collapsed" == $('#col_btn').prop('class')){
+                t_t = false;
+            }else{
+                t_t = true;
+                var old_text = $("input#old_text").val(),
+                    new_text = $("input#new_text").val();
+            }
+            if(t_t == false){
+                $.ajax({
+                    type: 'get',
+                    url: '/ajax_post_form/',
+                    data: {
+                        name: name,
+                        url: url,
+                        email: email,
+                        boll: t_t,
+                        pk: pk_s
+                    },
+                    //cache: false,
+                    success: function() {
+                        // Enable button & show success message
+                        $("#sale_b").attr("disabled", false);
+                        $('#success').html("<div class='alert alert-success'>");
+                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-success')
+                            .append("<strong>Ваше сообщение отправлено. </strong>");
+                        $('#success > .alert-success')
+                            .append('</div>');
+
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    },
+                    error: function() {
+                        // Fail message
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-danger').append("<strong>Простите " + firstName + ", возникла ошибка при отправке. Попробуйте позже!");
+                        $('#success > .alert-danger').append('</div>');
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    }
+                })
+            }else{
+                $.ajax({
+                    type: 'get',
+                    url: '/ajax_post_form/',
+                    data: {
+                        name: name,
+                        url: url,
+                        email: email,
+                        old_text: old_text,
+                        new_text: new_text,
+                        boll: t_t,
+                        pk: pk_s
+                    },
+                    //cache: false,
+                    success: function() {
+                        // Enable button & show success message
+                        $("#sale_b").attr("disabled", false);
+                        $('#success').html("<div class='alert alert-success'>");
+                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-success')
+                            .append("<strong>Ваше сообщение отправлено. </strong>");
+                        $('#success > .alert-success')
+                            .append('</div>');
+
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    },
+                    error: function() {
+                        // Fail message
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-danger').append("<strong>Простите " + firstName + ", возникла ошибка при отправке. Попробуйте позже!");
+                        $('#success > .alert-danger').append('</div>');
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    }
+                })
+            }
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        }
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+// When clicking on Full hide fail/success boxes
+$('#name').focus(function() {
+    $('#success').html('');
+});
